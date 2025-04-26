496a014051f18759c0dcad50f86f2f0a5ff1e5c1
i.vukolov <i.vukolov@vrgaz.ru>
2025-04-26 17:57:07 +0300

    added PostViewSet, CommentViewSet, GroupViewSet

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,18 +1,36 @@
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
 
-from .views import PostViewSet, GroupViewSet, CommentViewSet
+from posts.models import Post, Comment, Group
+from api.serializers import PostSerializer, GroupSerializer, CommentSerializer
+from .permissions import IsAuthorOrReadOnly
 
-router = DefaultRouter()
-router.register(r'posts', PostViewSet, basename='post')
-router.register(
-    r'posts/(?P<post_id>.+)/comments',
-    CommentViewSet,
-    basename='comment'
-)
-router.register(r'groups', GroupViewSet, basename='group')
 
-urlpatterns = [
-    path('v1/', include('djoser.urls.jwt')),
-    path('v1/', include(router.urls)),
-]
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly, )
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthorOrReadOnly, )
+
+    def get_queryset(self):
+        post = self.get_post_queryset()
+        return Comment.objects.filter(post_id=post.id)
+
+    def perform_create(self, serializer):
+        post = self.get_post_queryset()
+        serializer.save(author=self.request.user, post=post)
+
+    def get_post_queryset(self):
+        return get_object_or_404(Post, pk=self.kwargs.get('post_id'))
--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -101,6 +101,7 @@ REST_FRAMEWORK = {
     ],
 }
 
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'AUTH_HEADER_TYPES': ('Bearer',),
