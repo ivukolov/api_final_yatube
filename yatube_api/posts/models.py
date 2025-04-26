05868ee89f9c15618ba361024f565f56d251f0cb
i.vukolov <i.vukolov@vrgaz.ru>
2025-04-26 17:03:52 +0300

    added Group and Follow models

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,13 +4,24 @@ from django.db import models
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField("Название группы", max_length=200)
+    slug = models.SlugField("Слаг", unique=True)
+    description = models.TextField("Описание")
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    text = models.TextField("Текст поста")
+    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
+        "Автор", User, on_delete=models.CASCADE, related_name="posts"
+    )
     image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
+        "Изображение", upload_to="posts/", null=True, blank=True
+    )
 
     def __str__(self):
         return self.text
@@ -18,9 +29,29 @@ class Post(models.Model):
 
 class Comment(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
+        "Автор", User, on_delete=models.CASCADE, related_name="comments"
+    )
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
+        "Пост", Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    group = models.ForeignKey(
+        "Группа",
+        Group,
+        on_delete=models.SET_NULL,
+        related_name="posts",
+        blank=True,
+        null=True,
+    )
+    text = models.TextField("Текст комментария")
     created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+        "Дата добавления", auto_now_add=True, db_index=True
+    )
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='following'
+    )
+    following = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='followers'
+    )
